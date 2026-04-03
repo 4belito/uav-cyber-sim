0e8439519f5925190bc5d8102580d4eb87c4bc19
4belito <abelmath89@gmail.com>
2026-04-02 19:58:03 -0500

    feat: initial SITL sandbox notebook without sim_vehicle (proof of concept)

--- simulator/helpers/processes.py
+++ simulator/helpers/processes.py
@@ -12,7 +12,7 @@ from subprocess import DEVNULL, Popen
 class SimProcess(StrEnum):
     """Enum for different types of simulation processes."""
 
-    ARDUPILOT = "ardupilot"
+    ARDUPILOT = "ardupilot"  # includes SITL and sim_vehicle
     LOGIC = "logic"
     GCS = "gcs"
     ADSB_SOCAT = "adsb_socat"
